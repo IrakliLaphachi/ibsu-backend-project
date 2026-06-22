1afd727a5378f3d46c3a33158a6f7f14e113c065
IrakliLaphachi <iraklilaphachi@gmail.com>
2026-06-22 22:36:22 +0400

    added repository files

---
+++ src/main/java/org/example/ecommerce/repository/CartRepository.java
@@ -0,0 +1,10 @@
+package org.example.ecommerce.repository;
+
+import org.example.ecommerce.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUserId(Long userId);
+}
---
+++ src/main/java/org/example/ecommerce/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package org.example.ecommerce.repository;
+
+import org.example.ecommerce.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserId(Long userId);
+    long countByUserId(Long userId);
+}
